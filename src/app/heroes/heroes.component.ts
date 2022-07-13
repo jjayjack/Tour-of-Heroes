1fe618c8753b4321a5bfe9a195af349093588d4a
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-13 11:29:51 -0500

    Refactoring to include message component, Message Service in public and private and updated click event to run on public heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  public heroes: Hero[] = [];
   public selectedHero?: Hero;
-  constructor(private heroService: HeroService) {}
+  public heroes: Hero[] = [];
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   public ngOnInit(): void {
     this.getHeroes();
   }
 
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  public selectHero(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      this.selectedHero = undefined;
-    } else {
-      this.selectedHero = hero;
-    }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
