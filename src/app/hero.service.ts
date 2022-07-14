9f30e9965b9c855ec92de9b20ba1d671c12062c1
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-14 10:38:36 -0500

    Adding Save Feature to Hero Detail Compoent and updating detail info to new information saved

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,10 @@ export class HeroService {
     private readonly messageService: MessageService
   ) {}
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   private log(message: string) {
     this.messageService.add(`HeroSevice: ${message}`);
   }
@@ -32,6 +36,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap((_) => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
   /**
