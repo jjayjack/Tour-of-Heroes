7bd0377f2ae408345c7dd9d9a9d9e089e4f94556
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-14 10:31:24 -0500

    Updated to include API call to get id of Hero and updates Error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -28,9 +28,11 @@ export class HeroService {
     );
   }
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
   /**
    * Handle Http operation that failed.
